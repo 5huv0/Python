656b774a0c6fd1db77383762ba5f119ff1572323
5huv0 <md88748874@gmail.com>
2024-04-03 00:30:31 +0600

    py

--- variables.py
+++ variables.py
@@ -3,3 +3,39 @@ age = 23
 hobby = "gaming"
 
 print("my name is :", name ,  ", my age is :", age, ", my hobby is :", hobby)
+
+print(type(hobby))
+
+
+# Hash for commenting
+
+
+# basic sum
+
+a = 2
+b = 4
+
+sum = a + b
+divide = a / b
+subtract = a - b
+multi = a * b
+
+print(sum , subtract, multi, divide)
+
+
+# Multiplying string with integer
+
+
+a = "6"
+c = "go"
+b = 8
+
+mult = (a*c*b)
+
+add =((a+c)*b)
+
+print(mult)
+
+print(add)
+
+
