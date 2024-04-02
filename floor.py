253d1ba53196836b881f8cda0e2cd8815a1350cc
5huv0 <md88748874@gmail.com>
2024-04-03 00:44:41 +0600

    py

---
+++ floor.py
@@ -0,0 +1,6 @@
+a = 122
+b = 7
+
+floor = a//b
+
+print(floor)
---
+++ remainder.py
@@ -0,0 +1,10 @@
+# In that case if only the denominator is negative the ans will be negative or otherwise it will always be positive
+
+a = 5
+b =2 
+c = -2
+
+remain = a % b
+remain1 = a % c
+
+print (remain, remain1)
