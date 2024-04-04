634bf23c35f98f2f0ab89e74c906c00d4d3adb92
5huv0 <md88748874@gmail.com>
2024-04-04 12:36:26 +0600

    basic prac of py

---
+++ prac.py
@@ -0,0 +1,33 @@
+# a = int(input("Input the value of a : "))
+# b = int(input("Input the value of b : "))
+
+# sum = a + b
+
+# print("The sum of A & B is : ", sum)
+
+#------------------------------------------------------------------------------
+
+# side = int(input("The side of the square is : "))
+
+# area = side * side
+
+# print("The area of the square is : ", area)
+
+#-------------------------------------------------------------------------------
+
+# float1 = float(input("The first floating point number is : "))
+# float2 = float(input("The second floating point number is : "))
+
+# avg = ((float1 + float2) / 2)
+
+# print("The average of floating numbers is : ", avg)
+
+#-------------------------------------------------------------------------------
+
+# a = int(input("Input A : "))
+# b = int(input("Input B : "))
+
+# if(a >= b ):
+#     print("True")
+# else:
+#     print("False")
