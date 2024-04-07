02b0fe3cd3e3522777dec592d79e6301b973cdb7
5huv0 <md88748874@gmail.com>
2024-04-07 12:17:05 +0600

    py

--- lists.py
+++ lists.py
@@ -1,19 +1,19 @@
-# students = ['rahim', 'karim', 'babu', 'saju']
-# marks = [79, 89, 65, 67]
+students = ['rahim', 'karim', 'babu', 'saju']
+marks = [79, 89, 65, 67]
 
-# print(students[0])
+print(students[0])
 
-# print(len(students[0]))
+print(len(students[0]))
 
 #--------------------------------------------------------------------------------------
 
 # Slicing of List 
 
-# name = ['shuvo', 'rahim', 'karim', 'tarek']
+name = ['shuvo', 'rahim', 'karim', 'tarek']
 
-# y = name[1 : 3]
+y = name[1 : 3]
 
-# print(y)
+print(y)
 
 #--------------------------------------------------------------------------------------
 
