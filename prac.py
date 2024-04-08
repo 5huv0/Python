e2bac62c9a957a65bb84ddde1687d280eb8e40fd
5huv0 <md88748874@gmail.com>
2024-04-08 18:01:58 +0600

    py

--- prac.py
+++ prac.py
@@ -284,3 +284,15 @@
 
 
 #--------------------------------------------------------------------------------
+
+
+# def factorial(n):
+#     factorial = 1
+#     i = 1
+
+#     while i <= n:
+#         factorial *= i
+#         i += 1
+#     return factorial    
+
+# print(factorial(5))
