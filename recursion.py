61035a715e382bb1fbd87dd818fa0b11984dcd02
5huv0 <md88748874@gmail.com>
2024-04-08 23:41:32 +0600

    py

--- recursion.py
+++ recursion.py
@@ -1,7 +1,17 @@
-def show(n):
-    if(n == 0):
-        return
-    print(n)
-    show(n - 1)
+# def show(n):
+#     if(n == 0):
+#         return
+#     print(n)
+#     show(n - 1)
 
-show(5)    
+# show(5)    
+
+
+#-----------------------------------------------------------
+
+def factorail(n):
+    if(n == 1 or n == 0):
+         return 1
+    return factorail(n - 1) * n
+
+print(factorail(int(input("Enter the value : "))))
