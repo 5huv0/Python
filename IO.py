533bd58a6e63f38852cc381955101eec75c0a8e4
5huv0 <md88748874@gmail.com>
2024-04-09 23:44:53 +0600

    py

--- IO.py
+++ IO.py
@@ -1,9 +1,28 @@
-# Open Read and Close files
+# Open Read files
 
-f = open("./files/demo.txt", "r")
+# f = open("./files/demo.txt", "r")
 
-data = f.read()
-data1 = f.readline()
-print(data, data1)
+# data = f.read()
+# data1 = f.readline()
+# print(data, data1)
+
+# f.close()
+
+# Open Write files
+
+# f = open("./files/demo.txt", "w")
+
+# data = f.write("This is a new line in my code")
+# print(data)
+
+# f.close()
+
+
+# Open Append files
+
+f = open("./files/demo.txt", "a")
+
+data = f.write("ok now add a new line i my file ")
+print(data)
 
 f.close()
