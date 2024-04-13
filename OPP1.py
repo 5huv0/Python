f28df16bbdd2cc540ee44b8faa41772c7c3d372e
5huv0 <md88748874@gmail.com>
2024-04-13 23:57:05 +0600

    py

--- OPP1.py
+++ OPP1.py
@@ -17,35 +17,40 @@
 #----------------------------------------------------------------
 
 # Constructor
-class Student:
-    collegeName = "DCC"
-    def __init__(self, fullname, marks):
-        self.name = fullname
-        self.marks = marks
-        print("you know nothing...")
 
-    def Welcome(self):
-        print("Welcome", self.name)    
+# class Student:
+#     collegeName = "DCC"
+#     def __init__(self, fullname, marks):
+#         self.name = fullname
+#         self.marks = marks
+#         print("you know nothing...")
 
-
-
-s1 = Student("mahi", 88)
-print(s1.name)
-print(s1.marks)
-print(s1.collegeName)
-
-s2 = Student("gobin", 67)
-s2.Welcome()
-print(s2.name)
-print(s2.marks)
+#     def Welcome(self):
+#         print("Welcome", self.name)    
 
 
-s3 = Student("hog",68)
-print(s3.name)
-print(s3.marks)
+
+# s1 = Student("mahi", 88)
+# print(s1.name)
+# print(s1.marks)
+# print(s1.collegeName)
+
+# s2 = Student("gobin", 67)
+# s2.Welcome()
+# print(s2.name)
+# print(s2.marks)
+
+
+# s3 = Student("hog",68)
+# print(s3.name)
+# print(s3.marks)
 
 
 #----------------------------------------------------------------
 
 # Static method that dont use self param
 
+class Cars:
+    @staticmethod
+    def Brands():
+        print("hi")
