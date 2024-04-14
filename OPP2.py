9c4cf25bf06910bcca85a42ff6bad24d7be8d59c
5huv0 <md88748874@gmail.com>
2024-04-14 11:54:51 +0600

    py

--- OPP2.py
+++ OPP2.py
@@ -13,23 +13,44 @@
 #----------------------------------------------------------------------
 # public and private attribute
 
-class Account:
-    def __init__(self, accNo, accPass):
-        self.accNo = accNo
-        self.__accPass = accPass
+# class Account:
+#     def __init__(self, accNo, accPass):
+#         self.accNo = accNo
+#         self.__accPass = accPass
 
-    def resetPass(self):
-        print(self.__accPass)    
+#     def resetPass(self):
+#         print(self.__accPass)    
 
 
-user1 = Account("1", "88745")
-user1.resetPass() # this is used in a class thats why working
-print(user1.__accPass) # this is not using inside the class that is why it is not working
+# user1 = Account("1", "88745")
+# user1.resetPass() # this is used in a class thats why working
+# print(user1.__accPass) # this is not using inside the class that is why it is not working
 
         
 #----------------------------------------------------------------------
 # Inheritance
 
+class Car:
+    color = "black"
+    @staticmethod
+    def start():
+        print("Car started....")
+
+    @staticmethod
+    def stop():
+        print("Car stopped!!!!")
+
+class Toyota(Car):
+    def __init__(self, name):
+        self.name = name
+
+class Supra(Toyota):
+    def __init__(self, type):
+        self.type = type
+
+car1 = Supra("diesel")
+car1.start()
+print(car1.color, car1.type)
 
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
