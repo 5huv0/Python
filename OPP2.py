3fc22ee12495c41601ee923f56ded5d6f7ca5c9c
5huv0 <md88748874@gmail.com>
2024-04-14 11:40:07 +0600

    py

--- OPP2.py
+++ OPP2.py
@@ -1,18 +1,36 @@
 #----------------------------------------------------------------------
 # del keyword
-class Student:
-    def __init__(self, name):
-        self.name = name
 
-s1 = Student("shuvo")
-print(s1)
-del s1
-print(s1)
+# class Student:
+#     def __init__(self, name):
+#         self.name = name
+
+# s1 = Student("shuvo")
+# print(s1)
+# del s1
+# print(s1)
         
 #----------------------------------------------------------------------
 # public and private attribute
 
+class Account:
+    def __init__(self, accNo, accPass):
+        self.accNo = accNo
+        self.__accPass = accPass
+
+    def resetPass(self):
+        print(self.__accPass)    
+
+
+user1 = Account("1", "88745")
+user1.resetPass() # this is used in a class thats why working
+print(user1.__accPass) # this is not using inside the class that is why it is not working
+
+        
 #----------------------------------------------------------------------
+# Inheritance
+
+
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
 #----------------------------------------------------------------------
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,1 @@
-n = int(input("Enter number : "))
-# sum = 1
-# i = 1
-
-# while i <= n:
-#     sum += i
-#     i += 1
- 
-# print("Total sum : ", sum)
+print(user1.__accPass)
